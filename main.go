d8911221de9a6bb208795a10ae5f92544e74e2d5
bricefriha <contact@bricefriha.dev>
2026-02-25 20:31:09 +0000

    add guidance for  create profile

--- main.go
+++ main.go
@@ -114,6 +114,7 @@ func showHelper() {
 	fmt.Println("switchto, help")
 	fmt.Println()
 	fmt.Println("Commands:")
+	fmt.Println("-c, --create [profileName]			    create a new profile")
 	fmt.Println("-st, --switchto [profileName]			switch to another profile")
 	fmt.Println("-h, --help 							show gpm user guidance ")
 
